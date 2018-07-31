71836f5351776e54d4352bc49b75afc02e021d32
illiaosmanov <illiaosmanov@gmail.com>
2018-07-31 20:29:26 +0300

    added folders

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', init);
+import $ from 'jquery';
 
-function init() {
-  const d = document;
-  const page = d.getElementById('homepage') || d.getElementById('about');
-  const id = page.getAttribute('id');
-
-  switch (id) {
-    case 'homepage': {
-      console.log('Hello, from Homepage');
-      break;
-    }
-    case 'about': {
-      console.log('Hello, from About page');
-      break;
-    }
-    default: {
-      // ...
-    }
-  }
-}
+$(function() {});
